4fb46a9d2ed2d312e937782a8cc52aa5527b66c1
Bernard Assan <mega.alpha100@gmail.com>
2026-07-24 16:03:26 +0000

    step: add an `all` step for building all examples
    use an option struct for `add_example` fn
    
    Signed-off-by: Bernard Assan <mega.alpha100@gmail.com>

--- build.zig
+++ build.zig
@@ -1,28 +1,28 @@
-const std = @import("std");
-
-pub fn build(b: *std.Build) void {
+pub fn build(b: *Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-
-    const zzz = b.addModule("zzz", .{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
 
     const tardy = b.dependency("tardy", .{
         .target = target,
         .optimize = optimize,
     }).module("tardy");
 
-    zzz.addImport("tardy", tardy);
-
     const secsock = b.dependency("secsock", .{
         .target = target,
         .optimize = optimize,
     }).module("secsock");
 
-    zzz.addImport("secsock", secsock);
+    const zzz = b.addModule("zzz", .{
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+        .imports = &.{
+            .{ .name = "tardy", .module = tardy },
+            .{ .name = "secsock", .module = secsock },
+        },
+    });
+
+    const all = b.step("all", "Build all Zzz examples");
 
     for ([_][]const u8{
         "basic",
@@ -32,21 +32,21 @@ pub fn build(b: *std.Build) void {
         "middleware",
         "sse",
         "tls",
-    }) |name| add_example(
-        b,
-        name,
-        target,
-        optimize,
-        zzz,
-    );
+    }) |name| add_example(b, .{
+        .name = name,
+        .target = target,
+        .optimize = optimize,
+        .zzz_module = zzz,
+        .all = all,
+    });
 
-    if (target.result.os.tag != .windows) add_example(
-        b,
-        "unix",
-        target,
-        optimize,
-        zzz,
-    );
+    if (target.result.os.tag != .windows) add_example(b, .{
+        .name = "unix",
+        .target = target,
+        .optimize = optimize,
+        .zzz_module = zzz,
+        .all = all,
+    });
 
     const tests = b.addTest(.{
         .name = "tests",
@@ -65,31 +65,39 @@ pub fn build(b: *std.Build) void {
 }
 
 fn add_example(
-    b: *std.Build,
-    name: []const u8,
-    target: std.Build.ResolvedTarget,
-    optimize: std.builtin.OptimizeMode,
-    zzz_module: *std.Build.Module,
+    b: *Build,
+    options: struct {
+        name: []const u8,
+        target: Build.ResolvedTarget,
+        optimize: std.lang.Optimize,
+        zzz_module: *Build.Module,
+        all: *Build.Step,
+    },
 ) void {
     const mod = b.createModule(.{
-        .root_source_file = b.path(b.fmt("examples/{s}/main.zig", .{name})),
-        .optimize = optimize,
-        .target = target,
+        .root_source_file = b.path(
+            b.fmt("examples/{s}/main.zig", .{options.name}),
+        ),
+        .optimize = options.optimize,
+        .target = options.target,
         .strip = false,
     });
-    mod.addImport("zzz", zzz_module);
+    mod.addImport("zzz", options.zzz_module);
 
     const example = b.addExecutable(.{
-        .name = name,
+        .name = options.name,
         .root_module = mod,
     });
 
-    const install_artifact = b.addInstallArtifact(example, .{});
-    b.getInstallStep().dependOn(&install_artifact.step);
+    const install_artifact = b.addInstallArtifact(
+        example,
+        .{},
+    );
+    options.all.dependOn(&install_artifact.step);
 
     const build_step = b.step(
-        b.fmt("{s}", .{name}),
-        b.fmt("Build zzz example ({s})", .{name}),
+        b.fmt("{s}", .{options.name}),
+        b.fmt("Build zzz example ({s})", .{options.name}),
     );
     build_step.dependOn(&install_artifact.step);
 
@@ -97,9 +105,12 @@ fn add_example(
     run_artifact.step.dependOn(&install_artifact.step);
 
     const run_step = b.step(
-        b.fmt("run_{s}", .{name}),
-        b.fmt("Run zzz example ({s})", .{name}),
+        b.fmt("run_{s}", .{options.name}),
+        b.fmt("Run zzz example ({s})", .{options.name}),
     );
     run_step.dependOn(&install_artifact.step);
     run_step.dependOn(&run_artifact.step);
 }
+
+const std = @import("std");
+const Build = std.Build;
