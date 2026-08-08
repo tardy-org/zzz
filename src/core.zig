b7b40de4a3626994ceebb7f8cc132289ca7e35b2
Bernard Assan <mega.alpha100@gmail.com>
2026-08-08 14:20:21 +0000

    rename TypedStorage to Storage
    Signed-off-by: Bernard Assan <mega.alpha100@gmail.com>

--- src/core.zig
+++ src/core.zig
@@ -1,5 +1,5 @@
 pub const string_map = @import("core/string_map.zig");
-pub const TypedStorage = @import("core/TypedStorage.zig");
+pub const Storage = @import("core/Storage.zig");
 pub const Args = @import("core/Args.zig").Args;
 pub const Pseudoslice = @import("core/Pseudoslice.zig");
 
--- src/core/Storage.zig
+++ src/core/Storage.zig
@@ -1,48 +1,50 @@
-pub const TypedStorage = @This();
+pub const Storage = @This();
 
+// arena: std.heap.ArenaAllocator.State,
 arena: std.heap.ArenaAllocator,
-storage: hash_map.AutoHashMapUnmanaged(
-    Key,
+map: hash_map.AutoHashMapUnmanaged(
+    Hash,
     *anyopaque,
 ),
 
-pub fn init(allocator: mem.Allocator) TypedStorage {
+pub fn init(allocator: mem.Allocator) Storage {
     return .{
         .arena = .init(allocator),
-        .storage = .empty,
+        .map = .empty,
     };
 }
 
-pub fn deinit(self: *TypedStorage) void {
-    self.arena.deinit();
+pub fn deinit(storage: *Storage) void {
+    storage.map.deinit(storage.arena.allocator());
+    storage.arena.deinit();
 }
 
 /// Clears the Storage.
-pub fn clear(self: *TypedStorage) void {
-    self.storage.clearAndFree(self.arena.allocator());
-    _ = self.arena.reset(.retain_capacity);
+pub fn clear(storage: *Storage) void {
+    storage.map.clearAndFree(storage.arena.allocator());
+    _ = storage.arena.reset(.retain_capacity);
 }
 
 /// Inserts a value into the Storage.
 /// It uses the given type as the K.
-pub fn put(self: *TypedStorage, comptime T: type, value: T) !void {
-    const allocator = self.arena.allocator();
+pub fn put(storage: *Storage, comptime T: type, value: T) !void {
+    const allocator = storage.arena.allocator();
     const ptr = try allocator.create(T);
     ptr.* = value;
     const type_id = comptime hash.Wyhash.hash(0, @typeName(T));
-    try self.storage.put(allocator, type_id, @ptrCast(ptr));
+    try storage.map.put(allocator, type_id, @ptrCast(ptr));
 }
 
 /// Extracts a value out of the Storage.
 /// It uses the given type as the K.
-pub fn get(self: *TypedStorage, comptime T: type) ?T {
+pub fn get(storage: *Storage, comptime T: type) ?T {
     const type_id = comptime hash.Wyhash.hash(0, @typeName(T));
-    const ptr = self.storage.get(type_id) orelse return null;
+    const ptr = storage.map.get(type_id) orelse return null;
     return @as(*T, @ptrCast(@alignCast(ptr))).*;
 }
 
-test "TypedStorage: Basic" {
-    var storage: TypedStorage = .init(testing.allocator);
+test "Storage: Basic" {
+    var storage: Storage = .init(testing.allocator);
     defer storage.deinit();
 
     // Test inserting and getting different types
@@ -72,7 +74,7 @@ test "TypedStorage: Basic" {
     try testing.expectEqual(200, storage.get(u32).?);
 }
 
-const Key = u64;
+const Hash = u64;
 
 const std = @import("std");
 const testing = std.testing;
