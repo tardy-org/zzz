0874842769c908e4cd4e54cacf9dd9cada94c053
Bernard Assan <mega.alpha100@gmail.com>
2026-08-14 19:11:25 +0000

    clean up Request
    rename self to match type name
    remove Request from type names
    make error return type explicit
    
    Signed-off-by: Bernard Assan <mega.alpha100@gmail.com>

--- src/http/Request.zig
+++ src/http/Request.zig
@@ -20,111 +20,123 @@ pub fn init(allocator: mem.Allocator) Request {
     };
 }
 
-pub fn deinit(self: *Request) void {
-    self.cookies.deinit();
-    self.headers.deinit();
+pub fn deinit(request: *Request) void {
+    request.cookies.deinit();
+    request.headers.deinit();
 }
 
-pub fn clear(self: *Request) void {
-    self.method = null;
-    self.uri = null;
-    self.body = null;
-    self.cookies.clear();
-    self.headers.clearRetainingCapacity();
+pub fn clear(request: *Request) void {
+    request.method = null;
+    request.uri = null;
+    request.body = null;
+    request.cookies.clear();
+    request.headers.clearRetainingCapacity();
 }
 
-const RequestParseOptions = struct {
-    request_bytes_max: u32,
-    request_uri_bytes_max: u32,
+const ParseOptions = struct {
+    max_request_bytes: u32,
+    max_uri_bytes: u32,
 };
 
 pub fn parse_headers(
-    self: *Request,
+    request: *Request,
     bytes: []const u8,
-    options: RequestParseOptions,
-) !void {
-    self.clear();
+    options: ParseOptions,
+) http.Error!void {
+    request.clear();
     var total_size: u32 = 0;
-    var lines = mem.tokenizeAny(u8, bytes, "\r\n");
+    var lines = mem.tokenizeAny(
+        u8,
+        bytes,
+        "\r\n",
+    );
 
     if (lines.peek() == null) {
-        return http.Error.MalformedRequest;
+        return error.MalformedRequest;
     }
 
     var parsing_first_line = true;
     while (lines.next()) |line| {
         total_size += @intCast(line.len);
 
-        if (total_size > options.request_bytes_max) {
-            return http.Error.ContentTooLarge;
+        if (total_size > options.max_request_bytes) {
+            return error.ContentTooLarge;
         }
 
         if (parsing_first_line) {
-            var chunks = mem.tokenizeScalar(u8, line, ' ');
+            var chunks = mem.tokenizeScalar(
+                u8,
+                line,
+                ' ',
+            );
 
             const method_string = chunks.next() orelse
-                return http.Error.MalformedRequest;
+                return error.MalformedRequest;
             const method = http.Method.parse(method_string) catch {
                 log.warn("invalid method: {s}", .{method_string});
-                return http.Error.InvalidMethod;
+                return error.InvalidMethod;
             };
 
             const uri_string = chunks.next() orelse
-                return http.Error.MalformedRequest;
-            if (uri_string.len >= options.request_uri_bytes_max)
-                return http.Error.URITooLong;
-            if (uri_string[0] != '/') return http.Error.MalformedRequest;
+                return error.MalformedRequest;
+            if (uri_string.len >= options.max_uri_bytes)
+                return error.URITooLong;
+            if (uri_string[0] != '/') return error.MalformedRequest;
 
             const version_string = chunks.next() orelse
-                return http.Error.MalformedRequest;
+                return error.MalformedRequest;
             if (!mem.eql(u8, version_string, "HTTP/1.1"))
-                return http.Error.HTTPVersionNotSupported;
-            self.set(.{ .method = method, .uri = uri_string });
+                return error.HTTPVersionNotSupported;
+            request.set(.{ .method = method, .uri = uri_string });
 
             // There shouldn't be anything else.
             if (chunks.next() != null) return http.Error.MalformedRequest;
             parsing_first_line = false;
         } else {
-            var header_iter = mem.tokenizeScalar(u8, line, ':');
+            var header_iter = mem.tokenizeScalar(
+                u8,
+                line,
+                ':',
+            );
             const key = header_iter.next() orelse
-                return http.Error.MalformedRequest;
+                return error.MalformedRequest;
             const value = mem.trimStart(
                 u8,
                 header_iter.rest(),
                 &.{' '},
             );
-            if (value.len == 0) return http.Error.MalformedRequest;
-            try self.headers.put(key, value);
+            if (value.len == 0) return error.MalformedRequest;
+            try request.headers.put(key, value);
         }
     }
 
-    if (self.headers.get("Cookie")) |cookies|
-        try self.cookies.parse_from_header(cookies);
+    if (request.headers.get("Cookie")) |cookies|
+        try request.cookies.parse_from_header(cookies);
 }
 
-pub const RequestSetOptions = struct {
+pub const SetOptions = struct {
     method: ?http.Method = null,
     uri: ?[]const u8 = null,
     body: ?[]const u8 = null,
 };
 
-pub fn set(self: *Request, options: RequestSetOptions) void {
+pub fn set(request: *Request, options: SetOptions) void {
     if (options.method) |method| {
-        self.method = method;
+        request.method = method;
     }
 
     if (options.uri) |uri| {
-        self.uri = uri;
+        request.uri = uri;
     }
 
     if (options.body) |body| {
-        self.body = body;
+        request.body = body;
     }
 }
 
 /// Should this specific Request expect to capture a body.
-pub fn expect_body(self: Request) bool {
-    return switch (self.method orelse return false) {
+pub fn expect_body(request: *const Request) bool {
+    return switch (request.method orelse return false) {
         .POST, .PUT, .PATCH => true,
         .GET, .HEAD, .DELETE, .CONNECT, .OPTIONS, .TRACE => false,
     };
@@ -138,21 +150,30 @@ test "Parse Request" {
         \\Accept: text/html
     ;
 
-    var request = Request.init(testing.allocator);
+    var request: Request = .init(testing.allocator);
     defer request.deinit();
 
     try request.parse_headers(request_text[0..], .{
-        .request_bytes_max = 1024,
-        .request_uri_bytes_max = 256,
+        .max_request_bytes = 1024,
+        .max_uri_bytes = 256,
     });
 
     try testing.expectEqual(.GET, request.method);
     try testing.expectEqualStrings("/", request.uri.?);
     try testing.expectEqual(.@"HTTP/1.1", request.version);
 
-    try testing.expectEqualStrings("localhost:9862", request.headers.get("Host").?);
-    try testing.expectEqualStrings("keep-alive", request.headers.get("Connection").?);
-    try testing.expectEqualStrings("text/html", request.headers.get("Accept").?);
+    try testing.expectEqualStrings(
+        "localhost:9862",
+        request.headers.get("Host").?,
+    );
+    try testing.expectEqualStrings(
+        "keep-alive",
+        request.headers.get("Connection").?,
+    );
+    try testing.expectEqualStrings(
+        "text/html",
+        request.headers.get("Accept").?,
+    );
 }
 
 test "Expect ContentTooLong Error" {
@@ -164,15 +185,21 @@ test "Expect ContentTooLong Error" {
     ;
 
     const large_content: [4096]u8 = @splat('a');
-    const request_text = std.fmt.comptimePrint(request_text_format, .{large_content});
+    const request_text = fmt.comptimePrint(
+        request_text_format,
+        .{large_content},
+    );
     var request: Request = .init(testing.allocator);
     defer request.deinit();
 
-    const err = request.parse_headers(request_text[0..], .{
-        .request_bytes_max = 128,
-        .request_uri_bytes_max = 64,
-    });
-    try testing.expectError(http.Error.ContentTooLarge, err);
+    const err = request.parse_headers(
+        request_text[0..],
+        .{ .max_request_bytes = 128, .max_uri_bytes = 64 },
+    );
+    try testing.expectError(
+        error.ContentTooLarge,
+        err,
+    );
 }
 
 test "Expect URITooLong Error" {
@@ -184,18 +211,18 @@ test "Expect URITooLong Error" {
     ;
 
     const large_content: [4096]u8 = @splat('a');
-    const request_text = std.fmt.comptimePrint(
+    const request_text = fmt.comptimePrint(
         request_text_format,
         .{large_content[0..]},
     );
     var request: Request = .init(testing.allocator);
     defer request.deinit();
 
-    const err = request.parse_headers(request_text[0..], .{
-        .request_bytes_max = 1024 * 1024,
-        .request_uri_bytes_max = 2048,
-    });
-    try testing.expectError(http.Error.URITooLong, err);
+    const err = request.parse_headers(
+        request_text[0..],
+        .{ .max_request_bytes = 1024 * 1024, .max_uri_bytes = 2048 },
+    );
+    try testing.expectError(error.URITooLong, err);
 }
 
 test "Expect Malformed when URI missing /" {
@@ -206,19 +233,19 @@ test "Expect Malformed when URI missing /" {
         \\Accept: text/html
     ;
     const content: [256]u8 = @splat('a');
-    const request_text = std.fmt.comptimePrint(
+    const request_text = fmt.comptimePrint(
         request_text_format,
         .{content[0..]},
     );
     var request: Request = .init(testing.allocator);
     defer request.deinit();
 
-    const err = request.parse_headers(request_text[0..], .{
-        .request_bytes_max = 1024,
-        .request_uri_bytes_max = 512,
-    });
+    const err = request.parse_headers(
+        request_text[0..],
+        .{ .max_request_bytes = 1024, .max_uri_bytes = 512 },
+    );
     try testing.expectError(
-        http.Error.MalformedRequest,
+        error.MalformedRequest,
         err,
     );
 }
@@ -234,12 +261,12 @@ test "Expect Incorrect HTTP Version" {
     var request: Request = .init(testing.allocator);
     defer request.deinit();
 
-    const err = request.parse_headers(request_text[0..], .{
-        .request_bytes_max = 1024,
-        .request_uri_bytes_max = 512,
-    });
+    const err = request.parse_headers(
+        request_text[0..],
+        .{ .max_request_bytes = 1024, .max_uri_bytes = 512 },
+    );
     try testing.expectError(
-        http.Error.HTTPVersionNotSupported,
+        error.HTTPVersionNotSupported,
         err,
     );
 }
@@ -255,17 +282,21 @@ test "Malformed string_map.AnyCase" {
     var request: Request = .init(testing.allocator);
     defer request.deinit();
 
-    const err = request.parse_headers(request_text[0..], .{
-        .request_bytes_max = 1024,
-        .request_uri_bytes_max = 512,
-    });
-    try testing.expectError(http.Error.MalformedRequest, err);
+    const err = request.parse_headers(
+        request_text[0..],
+        .{ .max_request_bytes = 1024, .max_uri_bytes = 512 },
+    );
+    try testing.expectError(
+        error.MalformedRequest,
+        err,
+    );
 }
 
 const log = std.log.scoped(.@"zzz/http/request");
 
 const std = @import("std");
 const mem = std.mem;
+const fmt = std.fmt;
 const assert = std.debug.assert;
 const testing = std.testing;
 
